e44dfda293b042ad7221539fbb41ced584fe3300
“Jessica <jessicayuridia@gmail.com>
2026-09-11 11:29:33 -0400

    adding gitignore

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # GLAB 385.2.1 - List & Methods
 
-print('testing')
+#To create the virtual env
+# python3 -m venv <VitualEnvName .venv>
+# python3 -m venv .venv
+
+# activating makes all modules you download go to the .venv folder,
+# andwhen you run your projects, it uses the modules from .env
+#To active the virual env:
+#scripts: source .env/Scripts/activate
+# bin source /venv/bin.activate
+
+#deactivate
+
+#To instal package in an activate env, use pip install
+#pip install <package>
+#pip install ipykernel
